fc8b842c2731bc170200b01c14a30efddd72ef1c
Gary Barzola <gnbarzol@espol.edu.ec>
2020-09-07 09:52:59 -0500

    Thunk para agregar funciones en los actions

--- src/Frontend/index.js
+++ src/Frontend/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-/*SERVER SIDE RENDERING*/
 import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
+
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'; //MiddleWare para agregar funciones que llamen a la api dentro de los actions
 import initialState from './initialState';
-/*SSR*/
 
 import reducer from './reducers';
 import App from './routes/App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const preloadedSate = window.__PRELOADED_STATE__;
+const store = createStore(reducer, preloadedSate, composeEnhancers(applyMiddleware(thunk)));
 const history = createBrowserHistory();
-const store = createStore(reducer, initialState);
+
+delete window.__PRELOADED_STATE__;
 
 ReactDOM.render(
   <Provider store={store}>
